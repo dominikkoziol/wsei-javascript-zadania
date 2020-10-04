44ce24593ede04a424e5c633506f733edb3f626f
Dominik Kozioł <dkoziol097@gmail.com>
2020-10-04 13:25:14 +0200

    First task

---
+++ 1.Początki/script.js
@@ -0,0 +1,16 @@
+// Task 1:
+console.log("Dominik");
+
+
+//Task 2:
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+//Task 3: 
+/* Zadziała */
+console.log("Hello World");
+//Błąd: nie ma ciapek, program nie wykona się, nie może znaleźć Hello i World a jeśli byłyby zmiennymi brakuje przecinka lub plusa
+console.log(Hello World);
+//Błąd nie ma ciapki na końcu, program się nie wykona
+console.log("Hello World);
