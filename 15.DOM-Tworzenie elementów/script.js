4d78cea0b9740db7411b0506e78f8a35c20112e0
Dominik Kozioł <dkoziol097@gmail.com>
2021-01-29 18:20:11 +0100

    ex 9, 10

--- 15.DOM-Tworzenie elementów/script.js
+++ 15.DOM-Tworzenie elementów/script.js
@@ -155,107 +155,230 @@ form.appendChild(inputHowManyTimesRepeat);
 document.body.appendChild(form);
 
 //ex 9:
+var breakex9 = document.createElement("br");
+document.body.appendChild(breakex9);
+
+
+var form2 = document.createElement("form");
+var inputId = 0;
+function createFormLine() {
+    var div = document.createElement("div");
+
+    var inputText = document.createElement("input");
+    inputText.setAttribute("placeholder", "Imię");
+    inputText.setAttribute("name", "name" + inputId);
+
+    var inputText2 = document.createElement("input");
+    inputText2.setAttribute("placeholder", "Nazwisko");
+    inputText2.setAttribute("name", "name" + inputId);
+
+    var inputText3 = document.createElement("input");
+    inputText3.setAttribute("placeholder", "Wiek");
+    inputText3.setAttribute("name", "name" + inputId);
+
+    var inputText4 = document.createElement("input");
+    inputText4.setAttribute("placeholder", "Ilość dzieci");
+    inputText4.setAttribute("name", "name" + inputId);
+
+    div.appendChild(inputText);
+    div.appendChild(inputText2);
+    div.appendChild(inputText3);
+    div.appendChild(inputText4);
+
+    form2.appendChild(div);
+    if(inputId == 0) document.body.appendChild(form2);
+    inputId++;
+}
+
+function createTable() {
+    var isTable = document.getElementById("table");
+    if(isTable) isTable.remove();
+
+    var table = document.createElement("table");
+    table.setAttribute("id", "table");
+    var headerRow = document.createElement("tr");
+
+    var columnName1 = document.createElement("th");
+    columnName1.innerHTML = "Imię";
+    var columnName2 = document.createElement("th");
+    columnName2.innerHTML = "Nazwisko";
+    var columnName3 = document.createElement("th");
+    columnName3.innerHTML = "Wiek";
+    var columnName4 = document.createElement("th");
+    columnName4.innerHTML = "Ilość dzieci";
+
+    headerRow.appendChild(columnName1);
+    headerRow.appendChild(columnName2);
+    headerRow.appendChild(columnName3);
+    headerRow.appendChild(columnName4);
+
+    table.appendChild(headerRow);
+
+    document.body.appendChild(table);
+    
+    for(let i = 0; i < inputId; i++) {
+        var elements = document.getElementsByName("name" + i);
+        var row = document.createElement("tr");
+        elements.forEach(e => {
+            var column = document.createElement("th");
+            column.innerHTML = e.value;
+            column.setAttribute("name", "column" + i);
+            row.appendChild(column);
+        });
+
+        var buttonRemove = document.createElement("button");
+        buttonRemove.innerHTML = "Usuń";
+      
+        
+        buttonRemove.addEventListener("click", function() {
+          var rowToDelete = document.getElementById("row" + i);
+          
+          rowToDelete.remove();
+        });
+        
+        var deleteColumn = document.createElement("th");
+        deleteColumn.appendChild(buttonRemove);
+        row.appendChild(deleteColumn);
+        row.setAttribute("id", "row"+ i);
+        table.appendChild(row);
+        console.log(elements);
+    }
+}
+
+
+
+createFormLine();
+
+var buttonMore = document.createElement("button");
+buttonMore.innerHTML = "Więcej";
+document.body.appendChild(buttonMore);
+
+buttonMore.addEventListener("click", function() {
+    createFormLine();
+});
+
+
+
+var buttonCreateTable = document.createElement("button");
+buttonCreateTable.innerHTML = "Stwórz tabelę";
+document.body.appendChild(buttonCreateTable);
+
+buttonCreateTable.addEventListener("click", function() {
+    createTable();
+    setUpperCaseToTableElementsEx9();
+});
+
+
 
 //ex 10: 
-
-//ex 11:
-function getStrings(anyString) {
-    var getNumbers = anyString.replace(/[^0-9]/g, "");
-    var product = getNumbers.length > 0 ? 1 : 0;
-    for (var i = 0; i < getNumbers.length; i++)
-        product = +getNumbers[i] * product;
-
-
-    console.log(product);
-
-    for (var i = 0; i < product; i++) {
-        var divWithText = document.createElement("div");
-        divWithText.innerHTML = anyString;
-        document.body.appendChild(divWithText);
-    }
-}
-
-getStrings("ala1 ma2 kota5");
-
-
-//ex 12:
-function getStringSaveToObject(someString) {
-    var obj = { text: someString };
-
-    if (obj.text.includes("Ala")) {
-        obj.text = obj.text.replace("Ala", "Ola");
-        console.log(obj.text);
-    }
-
-    else {
-        var divWithText = document.createElement("div");
-        divWithText.innerHTML = "Słowo Ala nie występuje w tekście.";
-        document.body.appendChild(divWithText);
-    }
-}
-
-getStringSaveToObject("Ola ma kota");
-
-//ex 13: 
-function getStringArrayAndCalc(strArr) {
-    var numArr = [];
-    for (var i = 0; i < strArr.length; i++) {
-        numArr.push(strArr[i].length);
-    }
-
-    return numArr;
-}
-
-function sumArr(numArr) {
-    var total = 0;
-    for (var i = 0; i < numArr.length; i++) {
-        total += numArr[i];
-    }
-    return total;
-}
-
-function avgArr(sum, arrLength) {
-    return sum / arrLength;
-}
-
-var strArr = ["kot", "adam", "kowalski", "kasza"];
-var numArr = getStringArrayAndCalc(strArr);
-var total = sumArr(numArr);
-var avg = avgArr(total, numArr.length);
-
-console.log(avg);
-
-//ex 14: 
-var objEx14 = {
-    name: '',
-    surname: '',
-    age: ''
-}
-
-function rewriteObj() {
-    objEx14.name = "Jacek";
-    objEx14.surname = "Kowalski";
-    objEx14.age = "32"
-
-    objEx14.nameLength = objEx14.name.length;
-    objEx14.surnameLength = objEx14.surname.length;
-    objEx14.ageLength = objEx14.age.length;
-
-    if (objEx14.nameLength > 5 || objEx14.surnameLength > 5 || objEx14.ageLength > 5) {
-        var buttonOnEvent = document.createElement("button");
-        buttonOnEvent.innerHTML = "Resetuj obiekt";
-        document.body.appendChild(buttonOnEvent);
-
-        buttonOnEvent.addEventListener("click", function () {
-            objEx14 = {
-                name: '',
-                surname: '',
-                age: ''
-            }       
-
+function setUpperCaseToTableElementsEx9() {
+    for(var i = 0; i < inputId; i++) {
+        var elements = document.getElementsByName("column" + i);
+        elements.forEach(e => {
+            if(e.innerHTML) e.innerHTML = e.innerHTML[0].toUpperCase() + e.innerHTML.slice(1);
         });
 
     }
 }
 
-rewriteObj();
+// //ex 11:
+// function getStrings(anyString) {
+//     var getNumbers = anyString.replace(/[^0-9]/g, "");
+//     var product = getNumbers.length > 0 ? 1 : 0;
+//     for (var i = 0; i < getNumbers.length; i++)
+//         product = +getNumbers[i] * product;
+
+
+//     console.log(product);
+
+//     for (var i = 0; i < product; i++) {
+//         var divWithText = document.createElement("div");
+//         divWithText.innerHTML = anyString;
+//         document.body.appendChild(divWithText);
+//     }
+// }
+
+// getStrings("ala1 ma2 kota5");
+
+
+// //ex 12:
+// function getStringSaveToObject(someString) {
+//     var obj = { text: someString };
+
+//     if (obj.text.includes("Ala")) {
+//         obj.text = obj.text.replace("Ala", "Ola");
+//         console.log(obj.text);
+//     }
+
+//     else {
+//         var divWithText = document.createElement("div");
+//         divWithText.innerHTML = "Słowo Ala nie występuje w tekście.";
+//         document.body.appendChild(divWithText);
+//     }
+// }
+
+// getStringSaveToObject("Ola ma kota");
+
+// //ex 13: 
+// function getStringArrayAndCalc(strArr) {
+//     var numArr = [];
+//     for (var i = 0; i < strArr.length; i++) {
+//         numArr.push(strArr[i].length);
+//     }
+
+//     return numArr;
+// }
+
+// function sumArr(numArr) {
+//     var total = 0;
+//     for (var i = 0; i < numArr.length; i++) {
+//         total += numArr[i];
+//     }
+//     return total;
+// }
+
+// function avgArr(sum, arrLength) {
+//     return sum / arrLength;
+// }
+
+// var strArr = ["kot", "adam", "kowalski", "kasza"];
+// var numArr = getStringArrayAndCalc(strArr);
+// var total = sumArr(numArr);
+// var avg = avgArr(total, numArr.length);
+
+// console.log(avg);
+
+// //ex 14: 
+// var objEx14 = {
+//     name: '',
+//     surname: '',
+//     age: ''
+// }
+
+// function rewriteObj() {
+//     objEx14.name = "Jacek";
+//     objEx14.surname = "Kowalski";
+//     objEx14.age = "32"
+
+//     objEx14.nameLength = objEx14.name.length;
+//     objEx14.surnameLength = objEx14.surname.length;
+//     objEx14.ageLength = objEx14.age.length;
+
+//     if (objEx14.nameLength > 5 || objEx14.surnameLength > 5 || objEx14.ageLength > 5) {
+//         var buttonOnEvent = document.createElement("button");
+//         buttonOnEvent.innerHTML = "Resetuj obiekt";
+//         document.body.appendChild(buttonOnEvent);
+
+//         buttonOnEvent.addEventListener("click", function () {
+//             objEx14 = {
+//                 name: '',
+//                 surname: '',
+//                 age: ''
+//             }
+
+//         });
+
+//     }
+// }
+
+// rewriteObj();
